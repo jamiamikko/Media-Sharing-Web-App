5fccf6f0dfddec61abbb6611e2a85352bc75abbc
Mikko Jämiä <jamiamikko@Mikko-MacBook-Pro-5.local>
2016-11-22 19:12:05 +0200

    Small fixes

--- wireframe_html/js/login_page.js
+++ wireframe_html/js/login_page.js
@@ -4,23 +4,23 @@ var login_form = document.getElementById("login");
 var signup_form = document.getElementById("signup");
 
 login_button.onclick = function(){
-	if (signup_button.className != "clicked")
+	if (signup_button.className != "red-button clicked")
 	{
 		event.preventDefault();
 	} 
-	signup_button.className = "clicked";
-	login_button.className = "";
+	signup_button.className = "red-button clicked";
+	login_button.className = "red-button";
 	login_form.className = "show";
 	signup_form.className = "";
 }
 
 signup_button.onclick = function(){
-	if (login_button.className != "clicked")
+	if (login_button.className != "red-button clicked")
 	{
 		event.preventDefault();
 	}
-	signup_button.className = "";
-	login_button.className = "clicked";
+	signup_button.className = "red-button";
+	login_button.className = "red-button clicked";
 	login_form.className = "";
 	signup_form.className = "show";
 }
