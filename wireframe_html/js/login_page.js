e2621c45ae6bbfc6fdeeddba32590cebd91c2b93
Mikko Jämiä <jamiamikko@Mikko-MacBook-Pro-5.local>
2016-11-22 19:13:03 +0200

    Indentations

--- wireframe_html/js/login_page.js
+++ wireframe_html/js/login_page.js
@@ -3,24 +3,22 @@ var signup_button = document.getElementById("signup_button");
 var login_form = document.getElementById("login");
 var signup_form = document.getElementById("signup");
 
-login_button.onclick = function(){
-	if (signup_button.className != "red-button clicked")
-	{
-		event.preventDefault();
-	} 
-	signup_button.className = "red-button clicked";
-	login_button.className = "red-button";
-	login_form.className = "show";
-	signup_form.className = "";
+login_button.onclick = function() {
+    if (signup_button.className != "red-button clicked") {
+        event.preventDefault();
+    }
+    signup_button.className = "red-button clicked";
+    login_button.className = "red-button";
+    login_form.className = "show";
+    signup_form.className = "";
 }
 
-signup_button.onclick = function(){
-	if (login_button.className != "red-button clicked")
-	{
-		event.preventDefault();
-	}
-	signup_button.className = "red-button";
-	login_button.className = "red-button clicked";
-	login_form.className = "";
-	signup_form.className = "show";
+signup_button.onclick = function() {
+    if (login_button.className != "red-button clicked") {
+        event.preventDefault();
+    }
+    signup_button.className = "red-button";
+    login_button.className = "red-button clicked";
+    login_form.className = "";
+    signup_form.className = "show";
 }
