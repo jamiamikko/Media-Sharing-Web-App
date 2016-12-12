59ff0aeb26e26e0ead452717934e24694b1222ab
Mokhtarz <Mokhtarz@metropolia.fi>
2016-12-12 10:13:22 +0200

    Update login_page.js
    comments added

--- SchoolProject1/web/js/login_page.js
+++ SchoolProject1/web/js/login_page.js
@@ -1,8 +1,11 @@
-var login_button = document.getElementById("login_button");
+
+//Get reference to the buttons and forms
+var login_button = document.getElementById("login_button"); 
 var signup_button = document.getElementById("signup_button");
 var login_form = document.getElementById("login");
 var signup_form = document.getElementById("signup");
 
+//when a buton is clicked, the coresponding form will show, if the form already show, the button do nothing
 login_button.onclick = function() {
     if (signup_button.className != "red-button clicked") {
         event.preventDefault();
